0c6334d61796c3bb679186f1c01f21b6f5f4b304
Kazumi Miura <bluesky-dev@aoisora.red>
2023-12-16 20:21:20 +0900

    1st

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import time
+
+st.title('StreamLit 超入門')
+
+st.write('プログレスバー')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'Done!!'
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+  right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+expander.write('問い合わせ内容を書く')
+expander.write('問い合わせ内容を書く')
+expander.write('問い合わせ内容を書く')
+expander.write('問い合わせ内容を書く')
+
+
+
+
+
+
+
+
